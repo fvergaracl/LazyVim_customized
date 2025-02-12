27b3b2b18a1e62ed7c86638cf367383b15614bf4
fvergaracl <fvergaracl@gmail.com>
2025-02-12 11:00:20 +0100

    NERDtree added and some custom config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.cmd("NERDTree")
+    vim.cmd("vertical resize 30")
+    vim.cmd("wincmd l")
+    vim.cmd("split")
+    vim.cmd("terminal")
+    vim.cmd("horizontal resize 10")
+    vim.cmd("wincmd k")
+  end,
+})
