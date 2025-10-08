183eb0bf981f5b47af773bf828df2f3abebdf45a
fvergaracl <fvergaracl@gmail.com>
2025-10-08 09:39:11 +0200

    feat(moltem): New shortcuts for moltem plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,16 @@ vim.keymap.set("n", "gd", function()
 end, { noremap = true, silent = true, desc = "Go to Definition" })
 
 vim.keymap.set("n", "<leader>er", ":Neotree close | Neotree reveal<CR>", { desc = "Reset Neo-tree root" })
+
+-- The following keymaps are for moltem plugin
+
+-- Visual: ejecutar selección
+vim.keymap.set("v", "<leader>r", ":<C-u>MoltenEvaluateVisual<CR>", { desc = "Molten: run selection" })
+-- Normal: ejecutar línea actual
+vim.keymap.set("n", "<leader>rl", ":MoltenEvaluateLine<CR>", { desc = "Molten: run line" })
+-- Normal: ejecutar celda `# %%`
+vim.keymap.set("n", "<leader>rc", ":MoltenReevaluateCell<CR>", { desc = "Molten: run cell (# %%)" })
+-- Normal: archivo completo
+vim.keymap.set("n", "<leader>rf", ":MoltenEvaluateFile<CR>", { desc = "Molten: run file" })
+-- Normal: abrir salida
+vim.keymap.set("n", "<leader>ro", ":MoltenEnterOutput<CR>", { desc = "Molten: open output" })
